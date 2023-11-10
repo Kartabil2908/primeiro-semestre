98bfc994422da0efcee025d3efebff5a37ffdc27
Bernardo Ladeira Kartabil <beladeira1.000@gmail.com>
2023-11-10 15:23:17 -0300

    Exclusão do arquivo .pynb

--- LIP/Python-102 --Analise-dos-gastos-publicos-do-estado-de-minas-gerais/princ.py
+++ LIP/Python-102 --Analise-dos-gastos-publicos-do-estado-de-minas-gerais/princ.py
@@ -1,1 +1,12 @@
-print('Hello World!')
+import plotly
+import pandas as pd
+dados2019 = pd.read_excel('./dados/xlsx/consulta_despesas_2019.xlsx')
+print(dados2019.head())
+dados2020 = pd.read_excel('./dados/xlsx/consulta_despesas_2020.xlsx')
+print(dados2020.head())
+dados2021 = pd.read_excel('./dados/xlsx/consulta_despesas_2021.xlsx')
+print(dados2021.head())
+dados2022 = pd.read_excel('./dados/xlsx/consulta_despesas_2022.xlsx')
+print(dados2022.head())
+dados2023 = pd.read_excel('./dados/xlsx/consulta_despesas_2023.xlsx')
+print(dados2023.head())
